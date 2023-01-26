e2b0807763dff930b5091ada16a09daec50133b8
Felip3FL <felip3.fl@gmail.com>
2023-01-26 09:42:06 -0300

    New local name color

---
+++ source/vs2022/FL_VS22_RED/Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FL_VS_RED
+{
+    internal class Test
+    {
+        public async Task<bool> oi(string valor)
+        {
+            var oioi = string.Empty;
+            var numero = 0;
+            var ver = true;
+
+            var reader = new StreamReader("");
+
+            if (reader.ReadToEnd() == null)
+                return default;
+                
+            try
+            {
+                if (ver)
+                {
+                    Console.WriteLine(oioi);
+                    Console.WriteLine(numero);
+                    Console.WriteLine("TEXT TEXT TEXT");
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+
+            return default;
+        }
+    }
+}
