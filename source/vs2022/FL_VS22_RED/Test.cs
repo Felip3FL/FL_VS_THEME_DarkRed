b74c8c7551c1e2db8098e313c33b4aa99d021a69
Felip3FL <felip3.fl@gmail.com>
2023-01-26 17:31:45 -0300

    Update test

--- source/vs2022/FL_VS22_RED/Test.cs
+++ source/vs2022/FL_VS22_RED/Test.cs
@@ -9,6 +9,13 @@ namespace FL_VS_RED
 {
     internal class Test
     {
+        public readonly String njhkdwaj;
+        private readonly String asdasd;
+        private readonly String retsdf;
+        private readonly String jhkferkj;
+
+        #region TESTE
+
         public async Task<bool> oi(string valor)
         {
             var oioi = string.Empty;
@@ -37,5 +44,40 @@ namespace FL_VS_RED
 
             return default;
         }
+
+        #endregion
+
+        #region TESTE22
+
+        public async Task<bool> oi2(string valor)
+        {
+            var oioi = string.Empty;
+            var numero = 0;
+            var ver = true;
+
+            var reader = new StreamReader("");
+
+            if (reader.ReadToEnd() == null)
+                return default;
+
+            try
+            {
+                if (ver)
+                {
+                    Console.WriteLine(oioi);
+                    Console.WriteLine(numero);
+                    Console.WriteLine("TEXT TEXT TEXT");
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+
+            return default;
+        }
+
+        #endregion
     }
 }
