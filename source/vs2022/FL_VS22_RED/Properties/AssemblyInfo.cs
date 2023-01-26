037f4767ad11545a38b7b8d482c2296ea049440a
Felip3FL <felip3.fl@gmail.com>
2023-01-26 09:23:11 -0300

    Add AssemblyInfo v1.23.1.26

---
+++ source/vs2022/FL_VS22_RED/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FL Red")]
+[assembly: AssemblyDescription("A Visual Studio 2022 Theme")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("FLex")]
+[assembly: AssemblyProduct("FL_VS_RED.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("96e48c6e-8e90-4b30-b2cb-7d4fbe861001")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.23.1.26")]
