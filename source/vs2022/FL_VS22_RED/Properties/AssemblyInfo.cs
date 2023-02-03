c4f62e0c3857f1522b55721c409caae46e314f43
Felip3FL <felip3.fl@gmail.com>
2023-02-03 13:34:44 -0300

    Update AssemblyInfo

--- source/vs2022/FL_VS22_RED/Properties/AssemblyInfo.cs
+++ source/vs2022/FL_VS22_RED/Properties/AssemblyInfo.cs
@@ -29,4 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.23.2.2")]
+[assembly: AssemblyVersion("1.23.2.3")]
+[assembly: AssemblyFileVersion("1.23.2.3")]
